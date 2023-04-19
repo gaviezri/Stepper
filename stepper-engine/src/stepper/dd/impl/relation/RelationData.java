226f14013da83f91bd5b3d525dbb87c1db56062b
Gal Aviezri <g.aviezri@gmail.com>
2023-04-19 23:36:44 +0300

    WIP - CSV Exporter

--- stepper-engine/src/stepper/dd/impl/relation/RelationData.java
+++ stepper-engine/src/stepper/dd/impl/relation/RelationData.java
@@ -1,23 +1,70 @@
 package stepper.dd.impl.relation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.CollationKey;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class RelationData {
+public class RelationData extends RelationDataDefinition{
 
-    private List<String> columns;
-    private List<SingleRow> rows;
+    private List<String> columnsNames;
+    private List<SingleRow> rows = new ArrayList<>();
+    private Map<String,List<String>> columns = new HashMap<>();
 
-    public RelationData(List<String> columns) {
-        this.columns = columns;
-        rows = new ArrayList<>();
+    int rowSize = 0;
+    int colSize = 0;
+    int totalSize = 0;
+
+    public enum Element {
+        ROW, COLUMN
     }
 
-    public List<String> getRowDataByColumnsOrder(int rowId) {
-        return new ArrayList<>();
+
+    public RelationData(List<String> columnsNames) {
+
+        this.columnsNames = columnsNames;
+        columnsNames.forEach(columnName -> columns.put(columnName, new ArrayList<>()));
+        rowSize = colSize = totalSize = 0;
     }
+
+    public List getDataFromRow(int index){
+        List<String> dataFromRow = new ArrayList<>();
+        for (String columnName : columnsNames) {
+            dataFromRow.add(columns.get(columnName).get(index));
+        }
+        return dataFromRow;
+    }
+    public List getDataFromColumn(String columnName){
+        return columns.get(columnName);
+    }
+    public String getDataFromCell(int rowIndex, String columnName){
+        return columns.get(columnName).get(rowIndex);
+    }
+    public void addRow(List<String> row){
+        for (int i = 0; i < row.size(); i++) {
+            columns.get(columnsNames.get(i)).add(row.get(i));
+            totalSize +=  row.size();
+            rowSize += row.size();
+        }
+    }
+    public void addColumn(String columnName, List<String> column){
+        columns.put(columnName, column);
+        rows.forEach(row -> row.addData(columnName, null));
+        columnsNames.add(columnName);
+        colSize += 1;
+        totalSize += column.size();
+    }
+
+    public int getRowSize() {
+        return rowSize;
+    }
+    public int getColSize() {
+        return colSize;
+    }
+    public int getTotalSize() {
+        return totalSize;
+    }
+
+    //TODO: ?? DELETE ??
 
     private static class SingleRow {
         private Map<String, String> data;
--- stepper-engine/src/stepper/step/impl/CSVExporter.java
+++ stepper-engine/src/stepper/step/impl/CSVExporter.java
@@ -1,6 +1,7 @@
 package stepper.step.impl;
 
 import stepper.dd.impl.DataDefinitionRegistry;
+import stepper.dd.impl.relation.RelationData;
 import stepper.dd.impl.relation.RelationDataDefinition;
 import stepper.flow.execution.context.StepExecutionContext;
 import stepper.flow.execution.logger.AbstractLogger;
@@ -8,6 +9,8 @@ import stepper.step.api.AbstractStepDefinition;
 import stepper.step.api.DataDefinitionDeclarationImpl;
 import stepper.step.api.DataNecessity;
 import stepper.step.api.StepResult;
+
+import java.util.List;
 
 public class CSVExporter extends AbstractStepDefinition {
     public CSVExporter() {
@@ -28,10 +31,18 @@ public class CSVExporter extends AbstractStepDefinition {
 
     @Override
     public StepResult invoke(StepExecutionContext context) {
-        RelationDataDefinition relation = context.getDataValue("SOURCE", RelationDataDefinition.class);
+        RelationData relation = (RelationData) context.getDataValue("SOURCE", RelationDataDefinition.class);
         AbstractLogger logger = context.getStepLogger(this);
         StringBuilder result;
-        //TODO: implement
+        //TODO: SOME LOGGINGS AND FAILSAFE
+        for (int i = 0; i < relation.getRowSize() ; i++) {
+            result = new StringBuilder();
+            for (int j = 0; j < relation.getColSize(); j++) {
+                result.append(relation.getDataFromRow(i))
+                        .append(",");
+            }
+            //logger.info(result.toString());
+        }
 
 
         return StepResult.NULL;

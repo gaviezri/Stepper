862ce4d9eb7547abc61771d4ddd6bf829fa338ac
Gal Aviezri <g.aviezri@gmail.com>
2023-04-20 16:44:03 +0300

    CSV EXPORTER - ADDED TO REGISTRY

--- stepper-engine/src/stepper/step/StepDefinitionRegistry.java
+++ stepper-engine/src/stepper/step/StepDefinitionRegistry.java
@@ -1,6 +1,7 @@
 package stepper.step;
 
 import stepper.step.api.StepDefinition;
+import stepper.step.impl.CSVExporter;
 import stepper.step.impl.FileDumperStep;
 import stepper.step.impl.HelloWorldStep;
 import stepper.step.impl.PersonDetailsStep;
@@ -8,7 +9,8 @@ import stepper.step.impl.PersonDetailsStep;
 public enum StepDefinitionRegistry {
     HELLO_WORLD(new HelloWorldStep()),
     PERSON_DETAILS(new PersonDetailsStep()),
-    FILE_DUMPER(new FileDumperStep())
+    FILE_DUMPER(new FileDumperStep()),
+    CSV_EXPORTER(new CSVExporter())
     ;
 
     private final StepDefinition stepDefinition;

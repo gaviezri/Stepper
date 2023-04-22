6c4d98f9bbf88ba8ce23a0cde930032a89e9a02d
Gal Aviezri <g.aviezri@gmail.com>
2023-04-22 10:26:35 +0300

    StepExecutionDataManager - WIP

---
+++ stepper-engine/src/stepper/step/execution/StepExecutionData.java
@@ -0,0 +1,26 @@
+package stepper.step.execution;
+
+import stepper.dd.api.AbstractDataDefinition;
+import stepper.step.api.StepDefinition;
+import stepper.step.api.enums.StepResult;
+
+import java.util.Map;
+
+public class StepExecutionData {
+
+    private StepResult stepResult;
+    private Number Duration;
+
+    public Number getDuration() {
+        return Duration;
+    }
+    public void setDuration(Number duration) {
+        Duration = duration;
+    }
+    public StepResult getStepResult() {
+        return stepResult;
+    }
+    public void setStepResult(StepResult stepResult) {
+        this.stepResult = stepResult;
+    }
+}
---
+++ stepper-engine/src/stepper/step/manager/StepExecutionDataManager.java
@@ -0,0 +1,36 @@
+package stepper.step.manager;
+
+import stepper.flow.execution.logger.AbstractLogger;
+import stepper.flow.execution.logger.step.StepExecutionLoggerImpl;
+import stepper.manager.api.DataManager;
+import stepper.step.api.StepDefinition;
+import stepper.step.api.enums.StepResult;
+import stepper.step.execution.StepExecutionData;
+
+public class StepExecutionDataManager implements DataManager {
+    private AbstractLogger stepLogger;
+    private StepExecutionData stepExecutionData;
+
+    public StepExecutionDataManager(String step){
+        stepLogger = new StepExecutionLoggerImpl(step);
+        stepExecutionData = new StepExecutionData();
+    }
+
+    public AbstractLogger getStepLogger() {
+        return stepLogger;
+    }
+
+    public Number getDuration() {
+        return stepExecutionData.getDuration();
+    }
+    public void setDuration(Number duration) {
+        stepExecutionData.setDuration(duration);
+    }
+    public StepResult getStepResult() {
+        return stepExecutionData.getStepResult();
+    }
+    public void setStepResult(StepResult stepResult) {
+        stepExecutionData.setStepResult(stepResult);
+    }
+
+}

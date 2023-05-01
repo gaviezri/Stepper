be2baa6366a016c1b0cf15249a6456b703fc3f15
Gal Aviezri <g.aviezri@gmail.com>
2023-05-01 14:42:56 +0300

    Option 4 DONE

--- stepper-engine/src/stepper/flow/execution/FlowExecution.java
+++ stepper-engine/src/stepper/flow/execution/FlowExecution.java
@@ -72,7 +72,10 @@ public class FlowExecution {
     }
 
 
-    public Long getDuration() {
+    public Duration getDuration() {
+        return duration;
+    }
+    public Long getDurationInMillis() {
         return duration.toMillis();
     }
 

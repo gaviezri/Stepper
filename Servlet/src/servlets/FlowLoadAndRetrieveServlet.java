72e3b666da7b6ba435dcc214c3e894aafbd3afaa
omriSR <omri.1996s@gmail.com>
2023-06-27 11:06:41 +0300

    filtering flows save point

--- Servlet/src/servlets/FlowLoadAndRetrieveServlet.java
+++ Servlet/src/servlets/FlowLoadAndRetrieveServlet.java
@@ -1,16 +1,24 @@
 package servlets;
 
+import communication.Role;
+import communication.UserSystemInfo;
 import dto.flow.FlowDefinitionDTO;
 import dto.flow.LoadDataDTO;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import javafx.util.Pair;
 import stepper.controller.EngineController;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 
 import static communication.Utils.*;
 
@@ -41,6 +49,22 @@ public class FlowLoadAndRetrieveServlet extends HttpServlet {
             case FLOW_DEFINITIONS_ENDPOINT:
                 System.out.println("a call to 'flows/definitions' endpoint was made...");
                 resp.setContentType(JSON_CONTENT_TYPE);
+
+                ServletContext context = getServletContext();
+
+                Function<Pair<HttpServletRequest,String>,String> cookieBaker =  (Function<Pair<HttpServletRequest,String>,String>)context.getAttribute(COOKIE_BAKER);
+                Integer userCookie = Integer.valueOf(cookieBaker.apply(new Pair(req,"ID")));
+
+                UserSystemInfo userInfo = ((Map<Integer,UserSystemInfo>) context.getAttribute(COOKIE_2_USER)).get(userCookie);
+
+                List<Role> userRoles = userInfo.getRoles();
+
+                Set<String> userAccessibleFlows = new HashSet<String>();
+
+                for (Role role: userRoles) {
+                    userAccessibleFlows.addAll(role.getFlows());
+                }
+
 
                 break;
 
